825001a882d000887760688469e2646ef1d71973
Josua Pangihutan Sitanggang <josuastg93@gmail.com>
2025-01-05 17:08:39 +0700

    fix role permission rpute

--- src/misc/CheckRolePermission.js
+++ src/misc/CheckRolePermission.js
@@ -1,7 +1,8 @@
-import { useProfileStore } from "@/stores/profile";
 
 const checkRolePermission = (permission) => {
-    const rolePermission = JSON.parse(sessionStorage.getItem('user_role_permission'));
+    const rolePermission = JSON.parse(sessionStorage.getItem('user_role_permission')) ||
+        { "gender": "", "role_id": "", "employee_contact_phone": "", "employee_email": "", "employee_id": "", "employee_name": "", "role_name": "" }
+        ;
     if (permission === 'all_enable') return true;
     else return permission === rolePermission.role_name
 
